25239d33873f74320ba0653d5a55f7632e87e65f
supakit <131539244+Supakit0944926053@users.noreply.github.com>
2026-03-29 18:14:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let total = 0;
+
+function addToCart(name, price) {
+  const cart = document.getElementById("cart");
+
+  const li = document.createElement("li");
+  li.textContent = name + " - " + price + " บาท ";
+
+  const btn = document.createElement("button");
+  btn.textContent = "ลบ";
+  btn.onclick = function () {
+    cart.removeChild(li);
+    total -= price;
+    document.getElementById("total").textContent = total;
+  };
+
+  li.appendChild(btn);
+  cart.appendChild(li);
+
+  total += price;
+  document.getElementById("total").textContent = total;
+}
+
+// ✅ แยกออกมา
+function clearCart() {
+  document.getElementById("cart").innerHTML = "";
+  total = 0;
+  document.getElementById("total").textContent = total;
+}
+
+// ✅ แยกออกมา
+function sendOrder() {
+  const items = document.querySelectorAll("#cart li");
+
+  if (items.length === 0) {
+    alert("ยังไม่มีสินค้า");
+    return;
+  }
+
+  let message = "🍰 ร้านขนมแม่เตย\n";
+  message += "🛒 ออเดอร์ลูกค้า\n";
+  message += "------------------\n";
+
+  items.forEach(item => {
+    let text = item.textContent.replace("ลบ", "").trim();
+    message += "• " + text + "\n";
+});
+
+  message += "------------------\n";
+  message += "💰 รวม: " + total + " บาท";
+
+  const url = "https://line.me/R/msg/text/?" + encodeURIComponent(message);
+window.open(url);
+}
